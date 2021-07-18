a79e8b1e96bf743a659f37535e4a452cdb68c13b
hchauhan7816 <hchauhan7816@gmail.com>
2021-07-18 12:16:03 +0530

    MVP

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ class ConstructTrie {
 class TrieOperations {
   constructor() {
     this.words = [];
-    this.suggestionWanted = 10; // default number of suggestions 10
+    this.suggestionWanted = 15; // default number of suggestions 10
   }
 
   canExtendThisWord(temp_node) {
@@ -167,8 +167,41 @@ class TrieOperations {
     }
   }
 
-  // todo Same thing for BFS
-  // SuggestionsUsingBFS here only to avoid plag
+  BFS(inp_val, node) {
+    let v = [inp_val];
+    let q = [{ inp_val, node }];
+
+    while (q.length) {
+      console.log("v is " + v.toString());
+      if (v.length >= this.suggestionWanted) {
+        this.words = v;
+        return;
+      }
+
+      let pre;
+      let n;
+      let x = q.shift();
+
+      pre = x.inp_val;
+      n = x.node;
+
+      for (let c in n.child) {
+        const isOk = n.child[c].leaf;
+
+        if (isOk) {
+          v.push(pre + c);
+        }
+
+        q.push({
+          inp_val: pre + c,
+          node: n.child[c],
+        });
+      }
+    }
+
+    this.words = v;
+    return;
+  }
 
   GetSuggestionsUsingDFS(inp_val, root) {
     this.words = [];
@@ -189,7 +222,7 @@ class TrieOperations {
 
     let ans = [];
 
-    if (node.leaf) {
+    if (foundWholeString && !(this.canExtendThisWord(node) && len == n)) {
       // Can add functionality here highlight or stop recommending this word as it is Already a valid string
       // Like in google search it skips if the word is correct , maybe like that??
       ans.push(maxWordFound);
@@ -202,6 +235,66 @@ class TrieOperations {
     if (this.canExtendThisWord(node) && len == n) {
       console.log("here " + maxWordFound + " ans :: " + ans);
       this.DFS(maxWordFound, node);
+      console.log("Now here");
+      console.log(this.words);
+
+      for (let i = 0; i < this.words.length; i++) {
+        ans.push(this.words[i]);
+      }
+
+      return ans;
+    } else {
+      if (len == n && node.leaf) {
+        // todo
+        // this is only valid string without any further extension possible like Jatin
+        // No other word exists that Jatin....xyz
+        // this is only for those cases
+
+        return ans;
+      } else {
+        // todo
+        // this string does not exists
+        // maybe adding the string into our list feature?
+
+        return ans;
+      }
+    }
+  }
+
+  GetSuggestionsUsingBFS(inp_val, root) {
+    console.log("BFS ONE");
+    this.words = [];
+    let checkInTrie = this.findInTrie(inp_val, root);
+    console.log(checkInTrie);
+    console.log(checkInTrie.len);
+    console.log("Was");
+
+    let len = checkInTrie.len,
+      maxWordFound = checkInTrie.maxStringObtained,
+      node = checkInTrie.temp_node,
+      foundWholeString = checkInTrie.foundWholeString;
+
+    let n = inp_val.length;
+
+    console.log(
+      checkInTrie[0] + " " + maxWordFound + " " + " " + foundWholeString
+    );
+
+    let ans = [];
+
+    // if (node.leaf) {
+    // Can add functionality here highlight or stop recommending this word as it is Already a valid string
+    // Like in google search it skips if the word is correct , maybe like that??
+    // ans.push(maxWordFound);
+    // }
+
+    console.log("123");
+    console.log(node.leaf);
+    console.log("ans :: " + ans);
+
+    if (this.canExtendThisWord(node) && len == n) {
+      console.log("here " + maxWordFound + " ans :: " + ans);
+      this.BFS(maxWordFound, node);
       console.log(this.words);
 
       for (let i = 0; i < this.words.length; i++) {
@@ -228,15 +321,53 @@ class TrieOperations {
   }
 }
 
-function testing() {
+function solve() {
+  let s = document.getElementById("inputString").value;
+  console.log("string s is " + s);
+
+  if (s.length == 0) {
+    return [];
+  }
+
   const autocomplete = new ConstructTrie(DB);
-  const result = JSON.stringify(autocomplete.root);
+  // const result = JSON.stringify(autocomplete.root);
 
   console.log("HERE");
-  console.log(result);
 
   const x = new TrieOperations();
-  const ans = x.GetSuggestionsUsingDFS("a", autocomplete.root);
+  const ans = x.GetSuggestionsUsingDFS(s, autocomplete.root);
+  // const ans = x.GetSuggestionsUsingBFS(s, autocomplete.root);
 
   console.log(ans.toString());
+
+  // console.log("here BFS STARTS");
+  // const hl = x.BFS(s, autocomplete.root);
+
+  // document.write(ans.toString());
+  // document.write(hl.toString());
+
+  return ans;
+}
+
+// Front End Related Stuff
+function opennav() {
+  document.getElementById("sidenav").style.width = "250px";
+}
+
+function closenav() {
+  document.getElementById("sidenav").style.width = "0";
+}
+
+function printList() {
+  const arr = solve();
+
+  var ansList = "<ul>";
+
+  for (let i = 0; i < arr.length; i++) {
+    ansList += "<li>" + arr[i] + "</li>";
+  }
+
+  ansList += "</ul>";
+  document.getElementById("showSuggestions").innerHTML = "";
+  document.getElementById("showSuggestions").innerHTML += ansList;
 }
